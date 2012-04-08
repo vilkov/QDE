516b646a12db6b761392c2c4456591cecf4b21b8
Dmitriy Vilkov <dav.daemon@gmail.com>
2012-04-08 12:55:47 +0400

    Set XDG_MENU_PREFIX

--- qde-session/src/application.cpp
+++ qde-session/src/application.cpp
@@ -1,9 +1,10 @@
 #include "application.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 
 Application::Application(int &argc, char **argv) :
-	QdeCore::Application(tr("qde-session"), tr("QDE Session manager"), tr(""), argc, argv, false)
+	QdeCore::Application(tr("qde-session"), tr("QDE Session"), tr("QDE session manager"), argc, argv, false)
 {
-
+	setenv("XDG_MENU_PREFIX", "qde-", 1);
 }
